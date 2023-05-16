107b16bc4ff715e6b463a300abaff0a225ae07ef
Oleg Evdokimov <olegevdk@gmail.com>
2023-05-16 10:07:05 +0300

    move keyframe interp into AnimationChannels

--- Wrappers/Animation.h
+++ Wrappers/Animation.h
@@ -44,6 +44,79 @@ namespace dmbrn
 			scales[time] = value;
 		}
 
+		float GetScaleFactor(float lastTimeStamp, float nextTimeStamp, float loc_time)
+		{
+			const float midWayLength = loc_time - lastTimeStamp;
+			const float framesDiff = nextTimeStamp - lastTimeStamp;
+			return midWayLength / framesDiff;
+		}
+
+		glm::vec3 mixPositions(float l_time)
+		{
+			auto [lb,ub] = positions.equal_range(l_time);
+
+			if(lb==positions.end())
+				lb = --positions.end();
+
+			if(lb!=positions.begin() && lb->first > l_time)
+				--lb;
+
+			if (ub == positions.end())
+				--ub;
+
+			float factor = 0.f;
+			if (lb != ub)
+				factor = GetScaleFactor(lb->first, ub->first, l_time);
+			else
+				factor = 0;
+
+			return glm::mix(lb->second, ub->second, factor);
+		}
+
+		glm::quat slerpRotation(float l_time)
+		{
+			auto [lb,ub] = rotations.equal_range(l_time);
+
+			if(lb==rotations.end())
+				lb = --rotations.end();
+
+			if(lb!=rotations.begin()&& lb->first > l_time)
+				--lb;
+
+			if (ub == rotations.end())
+				--ub;
+
+			float factor = 0.f;
+			if (lb != ub)
+				factor = GetScaleFactor(lb->first, ub->first, l_time);
+			else
+				factor = 0;
+
+			return glm::slerp(lb->second, ub->second, factor);
+		}
+
+		glm::vec3 mixScale(float l_time)
+		{
+			auto [lb,ub] = scales.equal_range(l_time);
+
+			if(lb==scales.end())
+				lb = --scales.end();
+
+			if(lb!=scales.begin() && lb->first > l_time)
+				--lb;
+
+			if (ub == scales.end())
+				--ub;
+
+			float factor = 0.f;
+			if (lb != ub)
+				factor = GetScaleFactor(lb->first, ub->first, l_time);
+			else
+				factor = 0;
+
+			return glm::mix(lb->second, ub->second, factor);
+		}
+
 	};
 
 	struct AnimationClip
@@ -59,11 +132,11 @@ namespace dmbrn
 				TransformComponent& ent_trans_c = chnls.first.getComponent<TransformComponent>();
 
 				if (!chnls.second.positions.empty())
-					ent_trans_c.position = mixPositions(cur_local_frame, chnls.second);
+					ent_trans_c.position = chnls.second.mixPositions(cur_local_frame);
 				if (!chnls.second.rotations.empty())
-					ent_trans_c.setQuat(slerpRotation(cur_local_frame, chnls.second));
+					ent_trans_c.setQuat(chnls.second.slerpRotation(cur_local_frame));
 				if (!chnls.second.scales.empty())
-					ent_trans_c.scale = mixScale(cur_local_frame, chnls.second);
+					ent_trans_c.scale = chnls.second.mixScale(cur_local_frame);
 
 				chnls.first.markTransformAsEdited(frame);
 			}
@@ -77,80 +150,6 @@ namespace dmbrn
 		float getDuration() const
 		{
 			return max - min;
-		}
-
-	private:
-		float GetScaleFactor(float lastTimeStamp, float nextTimeStamp, float animationTime)
-		{
-			const float midWayLength = animationTime - lastTimeStamp;
-			const float framesDiff = nextTimeStamp - lastTimeStamp;
-			return midWayLength / framesDiff;
-		}
-
-		glm::vec3 mixPositions(float l_time, const AnimationChannels& chnls)
-		{
-			auto [lb,ub] = chnls.positions.equal_range(l_time);
-
-			if(lb==chnls.positions.end())
-				lb = --chnls.positions.end();
-
-			if(lb!=chnls.positions.begin() && lb->first > l_time)
-				--lb;
-
-			if (ub == chnls.positions.end())
-				--ub;
-
-			float factor = 0.f;
-			if (lb != ub)
-				factor = GetScaleFactor(lb->first, ub->first, l_time);
-			else
-				factor = 0;
-
-			return glm::mix(lb->second, ub->second, factor);
-		}
-
-		glm::quat slerpRotation(float l_time, const AnimationChannels& chnls)
-		{
-			auto [lb,ub] = chnls.rotations.equal_range(l_time);
-
-			if(lb==chnls.rotations.end())
-				lb = --chnls.rotations.end();
-
-			if(lb!=chnls.rotations.begin()&& lb->first > l_time)
-				--lb;
-
-			if (ub == chnls.rotations.end())
-				--ub;
-
-			float factor = 0.f;
-			if (lb != ub)
-				factor = GetScaleFactor(lb->first, ub->first, l_time);
-			else
-				factor = 0;
-
-			return glm::slerp(lb->second, ub->second, factor);
-		}
-
-		glm::vec3 mixScale(float l_time, const AnimationChannels& chnls)
-		{
-			auto [lb,ub] = chnls.scales.equal_range(l_time);
-
-			if(lb==chnls.scales.end())
-				lb = --chnls.scales.end();
-
-			if(lb!=chnls.scales.begin() && lb->first > l_time)
-				--lb;
-
-			if (ub == chnls.scales.end())
-				--ub;
-
-			float factor = 0.f;
-			if (lb != ub)
-				factor = GetScaleFactor(lb->first, ub->first, l_time);
-			else
-				factor = 0;
-
-			return glm::mix(lb->second, ub->second, factor);
 		}
 	};
 }
